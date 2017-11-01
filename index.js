dc1c4f05f5ed7ed3d1947bfbab04f51a7b5c56ac
shatalovdm <innoshatalov@gmail.com>
2017-10-31 22:08:31 -0500

    Set up Google Auth

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStr = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+passport.use(
+	new GoogleStr(
+		{
+			clientID: keys.googleID,
+			clientSecret: keys.googleSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
