c906aed0470caa5eee7c49f70d9f4e328d764280
shatalovdm <innoshatalov@gmail.com>
2017-11-02 17:49:13 -0500

    Set up prod environment

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleID: process.env.GOOGLE_ID,
+	googleSecret: process.env.GOOGLE_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY,
+	facebookID: process.env.FACEBOOK_ID,
+	facebookSecret: process.env.FACEBOOK_SECRET
+};
