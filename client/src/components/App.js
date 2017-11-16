5a1bc415a6bb01d85b6a6ec389189d40043f80f0
shatalovdm <innoshatalov@gmail.com>
2017-11-15 21:13:34 -0600

    Fetch user on boot up

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 import Header from './Header';
 
 const Account = () => <div>Account</div>;
 const NewRace = () => <div>New Race</div>;
 const Home = () => <div>Home</div>;
 
-const App = props => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route exact path="/" component={Home} />
-					<Route path="/account" component={Account} />
-					<Route path="/races/new" component={NewRace} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-};
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Home} />
+						<Route path="/account" component={Account} />
+						<Route path="/races/new" component={NewRace} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
 
-export default App;
+export default connect(null, { fetchUser })(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 	render() {
-		return <div>Header</div>;
+		return (
+			<nav>
+				<div className="nav-wrapper">
+					<a href="#" className="brand-logo">
+						RunHub
+					</a>
+					<ul className="right">
+						<li>
+							<a href="collapsible.html">Login</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
 	}
 }
 
